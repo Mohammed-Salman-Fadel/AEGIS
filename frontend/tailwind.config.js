78c9616ad9221cf3afbb247210d5d9fd6d919526
alperozenli <alperozenli04@gmail.com>
2026-05-06 23:56:34 +0300

    update tailwind config and gitignore rules

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,25 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class', 
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'progress': 'progress 2s infinite linear',
+      },
+      keyframes: {
+        progress: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(100%)' },
+        }
+      },
+      colors: {
+        
+        zinc: {
+          950: '#09090b',
+        }
+      }
+    },
   },
   plugins: [],
 }
